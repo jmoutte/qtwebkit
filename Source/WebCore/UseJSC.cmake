c7b3cb2d6a413ae5f58c0af91dd910cb6e3a4181
commit-queue@webkit.org <commit-queue@webkit.org>
2013-11-08 23:36:31 +0000

    Checking for TypeError in RTCPeerConnection object creation https://bugs.webkit.org/show_bug.cgi?id=124049
    Patch by Thiago de Barros Lacerda <thiago.lacerda@openbossa.org> on 2013-11-08
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    If invalid parameters are passed on RTCPeerConnection creation we must throw a TypeError exception.
    According to the spec it requires a Dictionary argument, the RTCConfiguration, which is mandatory.
    
    Please notice that this patch does not make every tests run as expected,
    RTCPeerConnectionHandlerMock needs to be update to deal with contraints.
    
    Existing tests were updated.
    
    * GNUmakefile.list.am:
    * Modules/mediastream/RTCPeerConnection.idl:
    * UseJSC.cmake:
    * WebCore.vcxproj/WebCore.vcxproj:
    * WebCore.vcxproj/WebCore.vcxproj.filters:
    * WebCore.xcodeproj/project.pbxproj:
    * bindings/js/JSRTCPeerConnectionCustom.cpp: Added.
    (WebCore::JSRTCPeerConnectionConstructor::constructJSRTCPeerConnection):
    
    LayoutTests:
    
    Please notice that this patch does not make every tests run as expected,
    RTCPeerConnectionHandlerMock needs to be update to deal with contraints.
    
    * fast/mediastream/RTCPeerConnection-AddRemoveStream.html:
    * fast/mediastream/RTCPeerConnection-createAnswer.html:
    * fast/mediastream/RTCPeerConnection-createOffer.html:
    * fast/mediastream/RTCPeerConnection-datachannel.html:
    * fast/mediastream/RTCPeerConnection-dtmf.html:
    * fast/mediastream/RTCPeerConnection-events.html:
    * fast/mediastream/RTCPeerConnection-expected.txt:
    * fast/mediastream/RTCPeerConnection-ice-expected.txt:
    * fast/mediastream/RTCPeerConnection-ice.html:
    * fast/mediastream/RTCPeerConnection-localDescription.html:
    * fast/mediastream/RTCPeerConnection-onnegotiationneeded.html:
    * fast/mediastream/RTCPeerConnection-remoteDescription.html:
    * fast/mediastream/RTCPeerConnection-stats-expected.txt:
    * fast/mediastream/RTCPeerConnection-stats.html:
    * fast/mediastream/RTCPeerConnection-statsSelector-expected.txt:
    * fast/mediastream/RTCPeerConnection-statsSelector.html:
    * fast/mediastream/RTCPeerConnection.html:
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@158964 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Conflicts:
    	LayoutTests/ChangeLog
    	LayoutTests/fast/mediastream/RTCPeerConnection-AddRemoveStream.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-createAnswer.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-createOffer.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-datachannel.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-dtmf.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-events.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-expected.txt
    	LayoutTests/fast/mediastream/RTCPeerConnection-ice-expected.txt
    	LayoutTests/fast/mediastream/RTCPeerConnection-ice.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-localDescription.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-onnegotiationneeded.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-remoteDescription.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-stats-expected.txt
    	LayoutTests/fast/mediastream/RTCPeerConnection-stats.html
    	LayoutTests/fast/mediastream/RTCPeerConnection-statsSelector-expected.txt
    	LayoutTests/fast/mediastream/RTCPeerConnection-statsSelector.html
    	LayoutTests/fast/mediastream/RTCPeerConnection.html
    	Source/WebCore/ChangeLog
    	Source/WebCore/UseJSC.cmake

--- Source/WebCore/UseJSC.cmake
+++ Source/WebCore/UseJSC.cmake
@@ -267,6 +267,9 @@ endif ()
 
 if (ENABLE_MEDIA_STREAM)
     list(APPEND WebCore_SOURCES
+        bindings/js/JSMediaSourceStatesCustom.cpp
+        bindings/js/JSMediaStreamCapabilitiesCustom.cpp
+        bindings/js/JSRTCPeerConnectionCustom.cpp
         bindings/js/JSRTCStatsResponseCustom.cpp
     )
 endif ()
