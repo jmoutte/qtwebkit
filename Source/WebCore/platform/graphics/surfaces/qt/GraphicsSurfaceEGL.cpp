5a0523cfb4e9ad18fcbb4927c4aeb2141c776dc2
Miguel Gomez <magomez@igalia.com>
2014-05-15 15:21:32 +0200

    Do not flip image when painting from GraphicsSurface

--- Source/WebCore/platform/graphics/surfaces/qt/GraphicsSurfaceEGL.cpp
+++ Source/WebCore/platform/graphics/surfaces/qt/GraphicsSurfaceEGL.cpp
@@ -374,7 +374,7 @@ void GraphicsSurface::platformPaintToTextureMapper(TextureMapper* textureMapper,
     TextureMapperGL* texMapGL = static_cast<TextureMapperGL*>(textureMapper);
     TransformationMatrix adjustedTransform = transform;
     adjustedTransform.multiply(TransformationMatrix::rectToRect(FloatRect(FloatPoint::zero(), m_private->size()), targetRect));
-    texMapGL->drawTexture(platformGetTextureID(), TextureMapperGL::ShouldFlipTexture | TextureMapperGL::ShouldBlend, m_private->size(), targetRect, adjustedTransform, opacity);
+    texMapGL->drawTexture(platformGetTextureID(), TextureMapperGL::ShouldBlend, m_private->size(), targetRect, adjustedTransform, opacity);
 }
 
 uint32_t GraphicsSurface::platformFrontBuffer() const
