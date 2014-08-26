9f2d51b8f0da5ee892281ae9bfcfc9081bb59c70
Philippe Normand <philn@igalia.com>
2014-08-26 18:47:40 +0200

    Revert "plug a ref leak in StreamingClient"
    This patch is not right.
    This reverts commit 721e1e390162e3d38cf4d326903ee892b98a41f2.

--- Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
@@ -892,7 +892,7 @@ void webKitWebSrcSetMediaPlayer(WebKitWebSrc* src, WebCore::MediaPlayer* player)
 }
 
 StreamingClient::StreamingClient(WebKitWebSrc* src)
-    : m_src(static_cast<GstElement*>(gst_object_ref(src)))
+    : m_src(adoptGRef(static_cast<GstElement*>(gst_object_ref(src))))
 {
 }
 
