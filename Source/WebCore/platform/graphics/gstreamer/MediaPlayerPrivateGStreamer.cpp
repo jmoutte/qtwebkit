fberlakovich/r2c-webkit

[GTK] 8tracks.com triggers annoying pop-up window/installation of "About protocol source plugin" (GStreamer?) https://bugs.webkit.org/show_bug.cgi?id=133605
Reviewed by Carlos Garcia Campos.

Don't attempt to load blank URLs with the GStreamer media
player. Those URLs trigger the codec installer which is useless in
this scenario. This patch also renames some of the variables of
the ::load method, as suggested by Carlos.

* platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
(WebCore::MediaPlayerPrivateGStreamer::~MediaPlayerPrivateGStreamer):
Check video-sink validity before disconnecting its signal handlers.
(WebCore::MediaPlayerPrivateGStreamer::load): Don't load blank
URLs (about:blank).
* platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp:
(WebCore::MediaPlayerPrivateGStreamerBase::~MediaPlayerPrivateGStreamerBase):
Disconnect repaint handler only if it's valid.

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@170526 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -261,31 +261,35 @@ MediaPlayerPrivateGStreamer::~MediaPlayerPrivateGStreamer()
         m_playBin.clear();
     }
 
-    GRefPtr<GstPad> videoSinkPad = adoptGRef(gst_element_get_static_pad(m_webkitVideoSink.get(), "sink"));
-    g_signal_handlers_disconnect_by_func(videoSinkPad.get(), reinterpret_cast<gpointer>(mediaPlayerPrivateVideoSinkCapsChangedCallback), this);
+    if (m_webkitVideoSink) {
+        GRefPtr<GstPad> videoSinkPad = adoptGRef(gst_element_get_static_pad(m_webkitVideoSink.get(), "sink"));
+        g_signal_handlers_disconnect_by_func(videoSinkPad.get(), reinterpret_cast<gpointer>(mediaPlayerPrivateVideoSinkCapsChangedCallback), this);
+    }
 }
 
-void MediaPlayerPrivateGStreamer::load(const String& url)
+void MediaPlayerPrivateGStreamer::load(const String& urlString)
 {
     if (!initializeGStreamerAndRegisterWebKitElements())
         return;
 
-    URL kurl(URL(), url);
-    String cleanUrl(url);
+    URL url(URL(), urlString);
+    if (url.isBlankURL())
+        return;
 
     // Clean out everything after file:// url path.
-    if (kurl.isLocalFile())
-        cleanUrl = cleanUrl.substring(0, kurl.pathEnd());
+    String cleanURL(urlString);
+    if (url.isLocalFile())
+        cleanURL = cleanURL.substring(0, url.pathEnd());
 
     if (!m_playBin)
         createGSTPlayBin();
 
     ASSERT(m_playBin);
 
-    m_url = URL(URL(), cleanUrl);
-    g_object_set(m_playBin.get(), "uri", cleanUrl.utf8().data(), NULL);
+    m_url = URL(URL(), cleanURL);
+    g_object_set(m_playBin.get(), "uri", cleanURL.utf8().data(), NULL);
 
-    INFO_MEDIA_MESSAGE("Load %s", cleanUrl.utf8().data());
+    INFO_MEDIA_MESSAGE("Load %s", cleanURL.utf8().data());
 
     if (m_preload == MediaPlayer::None) {
         LOG_MEDIA_MESSAGE("Delaying load.");

jmoutte/qtwebkit

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@170526 268f45cc-cd09-0410-ab3c-d52691b4dbfc
Conflicts:
	Source/WebCore/ChangeLog
	Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
	Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -254,8 +254,10 @@ MediaPlayerPrivateGStreamer::~MediaPlayerPrivateGStreamer()
         m_playBin.clear();
     }
 
-    GRefPtr<GstPad> videoSinkPad = adoptGRef(gst_element_get_static_pad(m_webkitVideoSink.get(), "sink"));
-    g_signal_handlers_disconnect_by_func(videoSinkPad.get(), reinterpret_cast<gpointer>(mediaPlayerPrivateVideoSinkCapsChangedCallback), this);
+    if (m_webkitVideoSink) {
+        GRefPtr<GstPad> videoSinkPad = adoptGRef(gst_element_get_static_pad(m_webkitVideoSink.get(), "sink"));
+        g_signal_handlers_disconnect_by_func(videoSinkPad.get(), reinterpret_cast<gpointer>(mediaPlayerPrivateVideoSinkCapsChangedCallback), this);
+    }
 
     if (m_videoTimerHandler)
         g_source_remove(m_videoTimerHandler);
@@ -266,27 +268,29 @@ MediaPlayerPrivateGStreamer::~MediaPlayerPrivateGStreamer()
     m_audioTimerHandler = 0;
 }
 
-void MediaPlayerPrivateGStreamer::load(const String& url)
+void MediaPlayerPrivateGStreamer::load(const String& urlString)
 {
     if (!initializeGStreamerAndRegisterWebKitElements())
         return;
 
-    KURL kurl(KURL(), url);
-    String cleanUrl(url);
+    KURL kurl(KURL(), urlString);
+    if (kurl.isBlankURL())
+        return;
 
     // Clean out everything after file:// url path.
+    String cleanURL(urlString);
     if (kurl.isLocalFile())
-        cleanUrl = cleanUrl.substring(0, kurl.pathEnd());
+        cleanURL = cleanURL.substring(0, kurl.pathEnd());
 
     if (!m_playBin)
         createGSTPlayBin();
 
     ASSERT(m_playBin);
 
-    m_url = KURL(KURL(), cleanUrl);
-    g_object_set(m_playBin.get(), "uri", cleanUrl.utf8().data(), NULL);
+    m_url = KURL(KURL(), cleanURL);
+    g_object_set(m_playBin.get(), "uri", cleanURL.utf8().data(), NULL);
 
-    INFO_MEDIA_MESSAGE("Load %s", cleanUrl.utf8().data());
+    INFO_MEDIA_MESSAGE("Load %s", cleanURL.utf8().data());
 
     if (m_preload == MediaPlayer::None) {
         LOG_MEDIA_MESSAGE("Delaying load.");