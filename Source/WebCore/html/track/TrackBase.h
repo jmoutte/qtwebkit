f1a07fa5e3bf8e7786f0ee4443c1bcb6b53c8c1b
Julien Moutte <julien@fluendo.com>
2014-11-13 17:41:06 +0100

    All tracks now have a MediaSource member with associated functions.

--- Source/WebCore/html/track/TrackBase.h
+++ Source/WebCore/html/track/TrackBase.h
@@ -10,10 +10,10 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -35,6 +35,7 @@ namespace WebCore {
 
 class Element;
 class HTMLMediaElement;
+class SourceBuffer;
 
 class TrackBase : public RefCounted<TrackBase> {
 public:
@@ -58,6 +59,11 @@ public:
 
     virtual void clearClient() = 0;
 
+#if ENABLE(MEDIA_SOURCE)
+    SourceBuffer* sourceBuffer() const { return m_sourceBuffer; }
+    void setSourceBuffer(SourceBuffer* buffer) { m_sourceBuffer = buffer; }
+#endif
+
 protected:
     TrackBase(Type, const AtomicString& label, const AtomicString& language);
 
@@ -65,6 +71,10 @@ protected:
     virtual const AtomicString& defaultKindKeyword() const = 0;
 
     HTMLMediaElement* m_mediaElement;
+
+#if ENABLE(MEDIA_SOURCE)
+    SourceBuffer* m_sourceBuffer;
+#endif
 
 private:
     Type m_type;
